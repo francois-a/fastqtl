409e1ca0ec91c2f00dd57ca306db58ba355c2e31
Francois Aguet <francois@broadinstitute.org>
2016-06-08 23:31:17 -0400

    Added output_dir option

--- python/run_FastQTL_threaded.py
+++ python/run_FastQTL_threaded.py
@@ -8,7 +8,14 @@ import numpy as np
 import subprocess
 import gzip
 import multiprocessing as mp
+import contextlib
 
+@contextlib.contextmanager
+def cd(cd_path):
+    saved_path = os.getcwd()
+    os.chdir(cd_path)
+    yield
+    os.chdir(saved_path)
 
 def get_cmd(args, chunk):
     cmd = os.path.join(fastqtl_dir, 'bin', 'fastQTL')+' --vcf '+args.vcf+' --bed '+args.bed+' --window '+args.window\
@@ -42,7 +49,7 @@ parser.add_argument('bed', help='Phenotypes in UCSC BED extended format')
 parser.add_argument('prefix', help='Prefix for output file name')
 parser.add_argument('--covariates', default='', help='Covariates')
 parser.add_argument('--chunks', default='100', help='Number of chunks, minimum: #chromosomes')
-parser.add_argument('--permute', default=None, type=str, nargs='+', help='Number of permutations, e.g. [1000, 10000] (adaptive). Default: None')
+parser.add_argument('--permute', default=None, type=str, nargs='+', help='Number of permutations, e.g. [1000, 10000] (adaptive). Default: None (run nominal pass)')
 parser.add_argument('--window', default='1e6', help='Cis-window size. Default values is 1Mb (1e6).')
 parser.add_argument('--threshold', default='', help='Output only significant phenotype-variant pairs with a p-value below threshold (default 1)')
 parser.add_argument('--maf_threshold', default='0.0', help='Include only genotypes with minor allele frequency >=maf_threshold (default 0)')
@@ -50,43 +57,48 @@ parser.add_argument('--ma_sample_threshold', default='0', help='Include only gen
 parser.add_argument('--fdr', default=0.05, type=np.double)
 parser.add_argument('--seed', default=None, help='Random number generator seed')
 parser.add_argument('-t', '--threads', default='8', help='Number of threads')
+parser.add_argument('-o', '--output_dir', default='.', help='Output directory')
 args = parser.parse_args()
 fastqtl_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 
-print('Starting pool', flush=True)
-with mp.Pool(processes=int(args.threads)) as pool:
-    pdata_res = [pool.map_async(perm_worker, ((args,k),)) for k in np.arange(1,int(args.chunks)+1)]
-    pool.close()
-    pool.join()
-for res in pdata_res:  # check exit status
-    assert res.get()[0]==0
-print('done', flush=True)
+if not os.path.exists(args.output_dir):
+    os.makedirs(args.output_dir)
 
-if args.permute:
-    # merge chunks
-    print('Merging chunks ... ', end='', flush=True)
-    cmd = 'zcat '+args.prefix+'_chunk*.txt.gz | gzip -c -1 > '+args.prefix+'.txt.gz && rm '+args.prefix+'_chunk*.txt.gz'
-    subprocess.check_call(cmd, shell=True, executable='/bin/bash')
-    cmd = 'cat '+args.prefix+'_chunk*.log > '+args.prefix+'.egenes.log && rm '+args.prefix+'_chunk*.log'
-    subprocess.check_call(cmd, shell=True, executable='/bin/bash')
+with cd(args.output_dir):
+    print('Starting pool ... ', flush=True)
+    with mp.Pool(processes=int(args.threads)) as pool:
+        pdata_res = [pool.map_async(perm_worker, ((args,k),)) for k in np.arange(1,int(args.chunks)+1)]
+        pool.close()
+        pool.join()
+    for res in pdata_res:  # check exit status
+        assert res.get()[0]==0
     print('done.', flush=True)
+
+    if args.permute:
+        # merge chunks
+        print('Merging chunks ... ', end='', flush=True)
+        cmd = 'zcat '+args.prefix+'_chunk*.txt.gz | gzip -c -1 > '+args.prefix+'.txt.gz && rm '+args.prefix+'_chunk*.txt.gz'
+        subprocess.check_call(cmd, shell=True, executable='/bin/bash')
+        cmd = 'cat '+args.prefix+'_chunk*.log > '+args.prefix+'.egenes.log && rm '+args.prefix+'_chunk*.log'
+        subprocess.check_call(cmd, shell=True, executable='/bin/bash')
+        print('done.', flush=True)
     
-    # calculate q-values (R script also adds header)
-    print('Calculating q-values', flush=True)
-    cmd = 'Rscript '+os.path.join(fastqtl_dir, 'R', 'calculateSignificanceFastQTL.R')\
-        +' '+args.prefix+'.txt.gz '+str(args.fdr)+' '+args.prefix+'.egenes.txt.gz'
-    subprocess.check_call(cmd, shell=True, executable='/bin/bash')
-    os.remove(args.prefix+'.txt.gz')
-else:
-    # merge chunks
-    print('Merging chunks ... ', end='', flush=True)
-    with gzip.open('header_chunk.txt.gz', 'wb') as f:
-        f.write(b'gene_id\tvariant_id\ttss_distance\tpval_nominal\tslope\tslope_se\n')
-    cmd = 'zcat header_chunk.txt.gz '+args.prefix+'_chunk*.txt.gz | gzip -c -1 > '+args.prefix+'.txt.gz && rm '+args.prefix+'_chunk*.txt.gz'
-    subprocess.check_call(cmd, shell=True, executable='/bin/bash')
-    os.remove('header_chunk.txt.gz')
-    cmd = 'cat '+args.prefix+'_chunk*.log > '+args.prefix+'.allpairs.log && rm '+args.prefix+'_chunk*.log'
-    subprocess.check_call(cmd, shell=True, executable='/bin/bash')
-    print('done.', flush=True)
+        # calculate q-values (R script also adds header)
+        print('Calculating q-values', flush=True)
+        cmd = 'Rscript '+os.path.join(fastqtl_dir, 'R', 'calculateSignificanceFastQTL.R')\
+            +' '+args.prefix+'.txt.gz '+str(args.fdr)+' '+args.prefix+'.egenes.txt.gz'
+        subprocess.check_call(cmd, shell=True, executable='/bin/bash')
+        os.remove(args.prefix+'.txt.gz')
+    else:
+        # merge chunks
+        print('Merging chunks ... ', end='', flush=True)
+        with gzip.open('header_chunk.txt.gz', 'wb') as f:
+            f.write(b'gene_id\tvariant_id\ttss_distance\tpval_nominal\tslope\tslope_se\n')
+        cmd = 'zcat header_chunk.txt.gz '+args.prefix+'_chunk*.txt.gz | gzip -c -1 > '+args.prefix+'.txt.gz && rm '+args.prefix+'_chunk*.txt.gz'
+        subprocess.check_call(cmd, shell=True, executable='/bin/bash')
+        os.remove('header_chunk.txt.gz')
+        cmd = 'cat '+args.prefix+'_chunk*.log > '+args.prefix+'.allpairs.log && rm '+args.prefix+'_chunk*.log'
+        subprocess.check_call(cmd, shell=True, executable='/bin/bash')
+        print('done.', flush=True)
     
-    os.rename(args.prefix+'.txt.gz', args.prefix+'.allpairs.txt.gz')
+        os.rename(args.prefix+'.txt.gz', args.prefix+'.allpairs.txt.gz')
